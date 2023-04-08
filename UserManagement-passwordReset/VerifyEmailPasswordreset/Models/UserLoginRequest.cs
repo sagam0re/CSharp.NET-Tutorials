5c69847ef7d2edeb4e9aaf6000064459c729e0e6
Giorgi <giorgi.janelidze97@gmail.com>
2023-04-08 14:51:45 +0400

    added user managemant with password reset and email verifying functions

---
+++ UserManagement-passwordReset/VerifyEmailPasswordreset/Models/UserLoginRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VerifyEmailPasswordreset.Models
+{
+    public class UserLoginRequest
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
