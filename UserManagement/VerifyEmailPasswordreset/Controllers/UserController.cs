b3bc5adb4e86582bd43d603b146ed40c18de9590
Giorgi <giorgi.janelidze97@gmail.com>
2023-04-11 17:54:25 +0400

    init commit for userManagement

---
+++ UserManagement/VerifyEmailPasswordreset/Controllers/UserController.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace VerifyEmailPasswordreset.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegisterRequest request)
+        {
+           
+            if (_context.Users.Any(u => u.Email == request.Email))
+            {
+                return BadRequest("User already exists");
+            }
+
+            CreatePasswordHash(request.Password, out byte[] hash, out byte[] salt);
+
+            var user = new User
+            {
+                Email = request.Email,
+                PasswordHash = hash,
+                PasswordSalt = salt,
+                VerificationToken = CreateRandomToken()
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok("User created");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginRequest request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u =>u.Email == request.Email); 
+            if (user == null)
+            {
+                return BadRequest("User not found.");
+            }
+
+            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Password is incorrect");
+            }
+
+            if (user.VerifiedAt == null) 
+            {
+                return BadRequest("Not Verified!");
+            }
+            
+            return Ok("Welcome back");
+        }
+
+        [HttpPost("verify")]
+        public async Task<IActionResult> Verify(string token)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.VerificationToken == token);
+            if (user == null)
+            {
+                return BadRequest("Invalid token");
+            }
+
+            user.VerifiedAt = DateTime.Now;
+            await _context.SaveChangesAsync();
+
+            return Ok("User verified!");
+        }
+
+        [HttpPost("forgot-password")]
+        public async Task<IActionResult> ForgotPassword(string email)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+            if (user == null)
+            {
+                return BadRequest("User not found");
+            }
+
+            user.PasswordResetToken = CreateRandomToken();
+            user.ResetTokenExpires= DateTime.Now.AddDays(1);
+            await _context.SaveChangesAsync();
+
+            return Ok("You can change password");
+        }
+
+        [HttpPost("reset-password")]
+        public async Task<IActionResult> ResetPassword(ResetPasswordRequest request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.PasswordResetToken == request.Token);
+            if (user == null || user.ResetTokenExpires < DateTime.Now)
+            {
+                return BadRequest("Invalid token!");
+            }
+
+            CreatePasswordHash(request.Password, out byte[] hash, out byte[] salt);
+
+            user.PasswordHash= hash;
+            user.PasswordSalt= salt;
+            user.PasswordResetToken = null;
+            user.ResetTokenExpires = null;
+
+            await _context.SaveChangesAsync();
+
+            return Ok("Password successfully reset");
+        }
+
+
+        // Utils
+        private bool VerifyPasswordHash(string password, byte[] hash, byte[] salt)
+        {
+            using (var hmac = new HMACSHA512(salt))
+            {
+                var  computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+                return computedHash.SequenceEqual(hash);
+            }
+        }
+
+
+        private void CreatePasswordHash(string password, out byte[] hash, out byte[] salt)
+        {
+            using(var hmac = new HMACSHA512())
+            {
+                salt = hmac.Key;
+                hash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private string CreateRandomToken()
+        {
+            return Convert.ToHexString(RandomNumberGenerator.GetBytes(64));
+        }
+    }
+}
